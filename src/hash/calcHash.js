d60937e96e60bfc0232d63ba3230819b935f1d70
ilia-devfamily <i.mazhugin@dev.family>
2022-12-02 23:20:36 +0300

    (chore) completed hash assignment

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import crypto from 'node:crypto';
+import fs from 'fs/promises';
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const data = await fs.readFile(
+      'src/hash/files/fileToCalculateHashFor.txt',
+      { encoding: 'utf-8' }
+    );
+    const hash = crypto.createHash('sha256').update(data);
+    console.log(hash.digest('hex'));
+  } catch (e) {
+    throw new Error('Hash operation failed');
+  }
 };
 
 await calculateHash();
