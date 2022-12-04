321ce9c45f37d8a33dd969e2429ca33d76947207
ilia-devfamily <i.mazhugin@dev.family>
2022-12-04 18:39:37 +0300

    (chore) completed zip assignment

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { rm } from 'fs/promises';
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const source = fs.createReadStream('src/zip/files/fileToCompress.txt');
+    const destination = fs.createWriteStream('src/zip/files/archive.gz');
+    const gzip = createGzip();
+
+    await pipeline(source, gzip, destination).then(async () => {
+      await rm('src/zip/files/fileToCompress.txt');
+    });
+  } catch (error) {
+    throw new Error('Zip operation failed');
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { rm } from 'fs/promises';
+
 const decompress = async () => {
-    // Write your code here 
+  const source = fs.createReadStream('src/zip/files/archive.gz');
+  const destination = fs.createWriteStream('src/zip/files/fileToCompress.txt');
+  const unzip = createUnzip();
+
+  await pipeline(source, unzip, destination).then(async () => {
+    await rm('src/zip/files/archive.gz');
+  });
 };
 
 await decompress();
