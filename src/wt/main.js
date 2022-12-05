5185e5cdea7cbfeb402c4c73d2d9454bd3f69cf9
ilia-devfamily <i.mazhugin@dev.family>
2022-12-05 14:03:58 +0300

    (chore) complete wt assignment

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const startWorker = (num) =>
+  new Promise((resolve, reject) => {
+    const worker = new Worker('./src/wt/worker.js', { workerData: num });
+    worker.on('message', resolve);
+    worker.on('error', reject);
+  });
+
 const performCalculations = async () => {
-    // Write your code here
+  try {
+    const workers = cpus().map((_, index) => startWorker(10 + index));
+    const results = (await Promise.allSettled(workers)).map(
+      ({ status, value }) => ({
+        result: status,
+        value: value ?? null,
+      })
+    );
+    console.log(results);
+  } catch (error) {
+    throw new Error('WT operation failed');
+  }
 };
 
 await performCalculations();
